4825712cf49a3fa7c17c48d6c88cfe82dae0e565
Vitaliy Hreva <vitaliyhreva@gmail.com>
2019-12-24 18:35:13 +0200

    add task2

---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,16 @@
+let a = parseFloat(prompt("Please provide length of 'A' side"));
+let b = parseFloat(prompt("Please provide length of 'B' side"));
+let c = parseFloat(prompt("Please provide lenght of 'C' side"));
+
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+	console.log('input values should be ONLY numbers ')
+} else if (a === 0 || b === 0 || c === 0 ) {
+	console.log('A triangle must have 3 sides with a positive definite length')
+} else if (a === b && b === c && c === a) {
+	console.log('Eequivalent triangle');
+} else if (a === b || b === c || c === a) {
+	console.log('Isosceles triangle')
+} else {
+	console.log('Scalene  triangle')
+}
